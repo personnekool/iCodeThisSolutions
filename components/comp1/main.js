081396b24925c044419ee48069bff21cc49e9e7e
Javier Mauricio <javierma36@gmail.com>
2023-10-31 07:02:51 -0500

    Add js code to capture form changes and toggle buttons

--- components/comp1/main.js
+++ components/comp1/main.js
@@ -1,6 +1,21 @@
 (function(){
-  let ele = document.querySelectorAll(".option");
-  ele.forEach((x)=>{
-    x.classList.add("l-i");
+  let btn1 = document.querySelector('.btn-1');
+  let btn2 = document.querySelector('.btn-2');
+  let language = document.querySelector('#language');
+  let toggVideo = document.getElementById('video');
+  let toggProfile = document.getElementById('profile');
+
+  btn1.addEventListener('click',()=>{
+    let msg = `
+    Language: ${language.value}\n
+    Autoplay videos: ${toggVideo.checked}\n
+    Show profile photos: ${toggProfile.checked}
+    `;
+    alert(msg)
+  });
+  btn2.addEventListener('click',()=>{
+    toggVideo.checked = true;
+    toggProfile.checked = false;
+    language.value = "";
   });
 })();
